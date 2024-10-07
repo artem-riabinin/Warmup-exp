b7cc40273b7bfcef18a4a1d7246b218551049389
Artem <artem.riabinin@kaust.edu.sa>
2024-10-07 11:10:24 +0300

    no message

--- train.sh
+++ train.sh
@@ -4,7 +4,6 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=64G
 #SBATCH --time=00:30:00
 #SBATCH --output=output_%j.log
 
@@ -13,8 +12,6 @@ TOTAL_PROCESSES=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
 MASTER_ADDR=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
 
 MASTER_PORT=$(( ( RANDOM % 64512 ) + 1024 ))
-
-python data/openwebtext/prepare.py
 
 torchrun --nproc_per_node=$SLURM_NTASKS_PER_NODE \
          --nnodes=$SLURM_NNODES \
