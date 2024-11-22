2d38d7c6ee5c37a3548972fa5d3e175d5add56d3
Artem <artem.riabinin@kaust.edu.sa>
2024-11-23 00:58:41 +0300

    no message

--- train-Adam-nowarm-noclip.py
+++ train-Adam-nowarm-noclip.py
@@ -48,7 +48,7 @@ wandb_project = 'owt'
 wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'openwebtext'
-gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
+gradient_accumulation_steps = 5 * 2 # used to simulate larger batch sizes
 batch_size = 4 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
 # model
